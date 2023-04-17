fa10358e2f9f923e342921b9b8e2c1984ab5e04a
Stoyan Kolev <stoyan.d.kolev@gmail.com>
2023-04-17 21:40:45 +0300

    add ball and wall bounce

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import pygame
 BACKGROUND = (255, 250, 250)
 BLACK = (0, 0, 0)
 
+
 class Game:
     def __init__(self):
         pygame.init()
@@ -10,9 +11,12 @@ class Game:
         pygame.display.set_caption("Breakout!")
         self.clock = pygame.time.Clock()
         self.player = pygame.image.load('images/minus.png')
-        self.playerX = 600
-        self.playerY = 500
-
+        self.playerX, self.playerY = 600, 500
+        self.ball = pygame.image.load('images/ball.png')
+        self.ballX, self.ballY = 600, 500
+        self.velX = 0.5
+        self.velY = 0.5
+        self.bounds = self.screen.get_rect()
         self.main()
 
 
@@ -31,9 +35,25 @@ class Game:
             elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
                 self.move_player(1)
 
+            self.screen.blit(self.ball, (self.ballX, self.ballY))
+            self.ballX -= self.velX
+            self.ballY += self.velY
+            if self.ballX < self.bounds.left or self.ballX > self.bounds.right:
+                self.velX *= -1
+            if self.ballY < self.bounds.top or self.ballY > self.bounds.bottom:
+                self.velY *= -1
+
             self.screen.blit(self.player, (self.playerX, self.playerY))
             pygame.display.update()
 
 
     def move_player(self, pos_increment):
         self.playerX += pos_increment
+
+    # def check_ball(self):
+    #     global self.velX, self.velX
+
+            
+            
+
+
